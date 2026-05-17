166095b018839edbd143d6fa9e20ca85ee801b88
DarioKing71 <82949932+King7-lang@users.noreply.github.com>
2026-05-17 23:29:29 +0200

    Update game.c

--- game.c
+++ game.c
@@ -4,11 +4,28 @@
 #include "game.h"
 #include "file_handler.h"
 
-// Funkcija za crtanje ploce
+// Funkcija za vizualni ispis ploče s obojanim znakovima
 static void nacrtaj_plocu(char ploca[3][3]) {
     printf("\n      0   1   2 \n");
     for (int i = 0; i < 3; i++) {
-        printf(" %d | %c | %c | %c |\n", i, ploca[i][0], ploca[i][1], ploca[i][2]);
+        printf(" %d | ", i);
+        for (int j = 0; j < 3; j++) {
+            // Ako je na polju X, ispisujemo ga u crvenoj boji
+            if (ploca[i][j] == 'X') {
+                printf("\033[1;31mX\033[0m");
+            }
+            // Ako je na polju O, ispisujemo ga u plavoj boji
+            else if (ploca[i][j] == 'O') {
+                printf("\033[1;34mO\033[0m");
+            }
+            // Ako je polje prazno, ispisujemo običan razmak
+            else {
+                printf(" ");
+            }
+            
+            printf(" | ");
+        }
+        printf("\n");
         if (i < 2) printf("   |---|---|---|\n");
     }
 }
@@ -37,16 +54,19 @@ void play_game() {
         nacrtaj_plocu(ploca);
         char* aktivan = (trenutni_znak == 'X') ? res.player1 : res.player2;
 
-        printf("Igrac %s (%c), unesi red i stupac (npr. 0 1): ", aktivan, trenutni_znak);
-
-        // Jednostavan unos koordinata
+        // Kod ispisa teksta isto možemo obojati oznaku (%c) da igrač lakše vidi koji je znak
+        if (trenutni_znak == 'X') {
+            printf("Igrac %s (\033[1;31mX\033[0m), unesi red i stupac (npr. 0 1): ", aktivan);
+        } else {
+            printf("Igrac %s (\033[1;34mO\033[0m), unesi red i stupac (npr. 0 1): ", aktivan);
+        }
+        
         if (scanf("%d %d", &red, &stup) != 2) {
             printf("Greska pri unosu! Koristi brojeve.\n");
-            while (getchar() != '\n');
+            while (getchar() != '\n'); 
             continue;
         }
 
-        // Provjera jesu li koordinate unutar granica i je li polje slobodno
         if (red < 0 || red > 2 || stup < 0 || stup > 2 || ploca[red][stup] != ' ') {
             printf("Nevazeci potez!\n");
             continue;
@@ -59,12 +79,12 @@ void play_game() {
             nacrtaj_plocu(ploca);
             strcpy(res.winner, aktivan);
             printf("Pobjeda: %s!\n", res.winner);
-            save_result(&res); // Azuriranje podataka u datoteci
+            save_result(&res); 
             return;
         }
         trenutni_znak = (trenutni_znak == 'X') ? 'O' : 'X';
     }
-
+    
     printf("Izjednaceno!\n");
     strcpy(res.winner, "Izjednaceno");
     save_result(&res);
