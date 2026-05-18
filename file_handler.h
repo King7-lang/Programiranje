93f19b070f7ec251d22acc618fd1ef17a272200b
DarioKing71 <82949932+King7-lang@users.noreply.github.com>
2026-05-18 12:14:21 +0200

    Update file_handler.h

--- file_handler.h
+++ file_handler.h
@@ -1,10 +1,9 @@
-#ifndef FILE_HANDLER_H
-#define FILE_HANDLER_H
+#pragma once
 
 #include "game.h"
 
+// [Koncept 7: Organizacija izvornog koda - odvajanje prototipova u header]
 void save_result(const TrenutniRezultat* res);
 void display_results();
 void delete_single_result(int index);
-
-#endif
+void rucni_update_pobjeda(const char* ime, int nove_pobjede);
