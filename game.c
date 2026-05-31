7e75cfadebec593b6d2a1d92031ba56b64bc0786
DarioKing71 <82949932+King7-lang@users.noreply.github.com>
2026-05-31 20:00:59 +0200

    Update game.c

--- game.c
+++ game.c
@@ -5,6 +5,7 @@
 #include "game.h"
 #include "file_handler.h"
 
+// Makro za prenosivost: osigurava rad programa na Windowsima i Linuxu (Sleep/usleep).
 #ifdef _WIN32
 #include <windows.h>
 #define SLEEP_MS(ms) Sleep(ms)
@@ -13,11 +14,12 @@
 #define SLEEP_MS(ms) usleep((ms) * 1000)
 #endif
 
+// Kljucna rijec 'static' ogranicava vidljivost funkcije samo na ovu datoteku (enkapsulacija).
 static void nacrtaj_plocu(const char* const* ploca) {
-    system("cls"); // Cisti konzolu radi vizualnog osvjezavanja
+    system("cls"); // Osvjezavanje zaslona za vizualno cist prikaz igre.
 
     printf("\n\033[1;36m=== KRIZIC KRUZIC ===\033[0m\n");
-    printf("\n      0   1   2 \n");
+    printf("\n     0   1   2 \n");
     for (int i = 0; i < 3; i++) {
         printf(" %d \033[1;33m|\033[0m ", i);
         for (int j = 0; j < 3; j++) {
@@ -30,7 +32,6 @@ static void nacrtaj_plocu(const char* const* ploca) {
             else {
                 printf(" ");
             }
-
             printf(" \033[1;33m|\033[0m ");
         }
         printf("\n");
@@ -39,13 +40,13 @@ static void nacrtaj_plocu(const char* const* ploca) {
     printf("\n");
 }
 
-// [Koncept 6: Primjena kljucne rijeci static za funkcije]
-// [Koncept 14: Osiguranje parametara matrice pomocu kljucne rijeci const]
 static int provjeri_pobjednika(const char* const* ploca) {
+    // Provjera redaka i stupaca: klasicni algoritam za tic-tac-toe pobjedu.
     for (int i = 0; i < 3; i++) {
         if (ploca[i][0] != ' ' && ploca[i][0] == ploca[i][1] && ploca[i][1] == ploca[i][2]) return 1;
         if (ploca[0][i] != ' ' && ploca[0][i] == ploca[1][i] && ploca[1][i] == ploca[2][i]) return 1;
     }
+    // Provjera dijagonala.
     if (ploca[0][0] != ' ' && ploca[0][0] == ploca[1][1] && ploca[1][1] == ploca[2][2]) return 1;
     if (ploca[0][2] != ' ' && ploca[0][2] == ploca[1][1] && ploca[1][1] == ploca[2][0]) return 1;
     return 0;
@@ -56,7 +57,7 @@ void play_game() {
     int red, stup, potezi = 0;
     char trenutni_znak = 'X';
 
-    // [Koncept 16: Dinamicko zauzimanje memorije za dvodimenzionalno polje preko char**]
+    // Dinamicka alokacija matrice 3x3 na heapu koristeci dvostruki pokazivac.
     char** ploca = (char**)malloc(3 * sizeof(char*));
     if (ploca == NULL) {
         perror("Greska pri alokaciji redaka ploce");
@@ -66,13 +67,12 @@ void play_game() {
         ploca[i] = (char*)malloc(3 * sizeof(char));
         if (ploca[i] == NULL) {
             perror("Greska pri alokaciji stupaca ploce");
+            // Sigurnosni mehanizam: oslobadanje vec alocirane memorije u slucaju pogreske.
             for (int j = 0; j < i; j++) free(ploca[j]);
             free(ploca);
             return;
         }
-        for (int j = 0; j < 3; j++) {
-            ploca[i][j] = ' ';
-        }
+        for (int j = 0; j < 3; j++) ploca[i][j] = ' ';
     }
 
     system("cls");
@@ -81,17 +81,12 @@ void play_game() {
     printf("Player name 2 (\033[1;34mO\033[0m): "); scanf("%s", res.player2);
 
     while (potezi < 9) {
-        // Ovdje predajemo char** u funkciju koja prima const char* const* - kompajler to sigurno prosljeduje
         nacrtaj_plocu((const char* const*)ploca);
         const char* aktivan = (trenutni_znak == 'X') ? res.player1 : res.player2;
 
-        if (trenutni_znak == 'X') {
-            printf("Igrac \033[1;31m%s (X)\033[0m, unesi red i stupac (npr. 0 1): ", aktivan);
-        }
-        else {
-            printf("Igrac \033[1;34m%s (O)\033[0m, unesi red i stupac (npr. 0 1): ", aktivan);
-        }
+        printf("Igrac \033[1;36m%s (%c)\033[0m, unesi red i stupac (npr. 0 1): ", aktivan, trenutni_znak);
 
+        // Provjera ispravnosti unosa: sprjecavanje programa od "rusenja" pri pogresnom formatu.
         if (scanf("%d %d", &red, &stup) != 2) {
             printf("\033[1;31mGreska pri unosu! Koristi brojeve.\033[0m\n");
             while (getchar() != '\n');
@@ -111,13 +106,11 @@ void play_game() {
         if (provjeri_pobjednika((const char* const*)ploca)) {
             nacrtaj_plocu((const char* const*)ploca);
             strcpy(res.winner, aktivan);
+            printf("\n\033[1;32mPOBJEDNIK JE: %s!\033[0m\n", res.winner);
 
-            printf("\n\033[1;32m***************************************\033[0m\n");
-            printf("\033[1;32m     POBJEDNIK JE: %s!          \033[0m\n", res.winner);
-            printf("\033[1;32m***************************************\033[0m\n");
+            save_result(&res); // Pohrana rezultata u binarnu datoteku.
 
-            save_result(&res);
-
+            // Ciscenje memorije: oslobadanje svakog retka, pa glavnog pokazivaca.
             for (int i = 0; i < 3; i++) free(ploca[i]);
             free(ploca);
             ploca = NULL;
@@ -127,9 +120,7 @@ void play_game() {
     }
 
     nacrtaj_plocu((const char* const*)ploca);
-    printf("\n\033[1;33m***************************************\033[0m\n");
-    printf("\033[1;33m          IZJEDNACENO!                 \033[0m\n");
-    printf("\033[1;33m***************************************\033[0m\n");
+    printf("\n\033[1;33mIZJEDNACENO!\033[0m\n");
     strcpy(res.winner, "Izjednaceno");
     save_result(&res);
 
