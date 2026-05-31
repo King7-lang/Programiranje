5f15357eea13f141e31477b967b6c3fb19662587
DarioKing71 <82949932+King7-lang@users.noreply.github.com>
2026-05-31 20:00:43 +0200

    Update file_handler.h

--- file_handler.h
+++ file_handler.h
@@ -1,9 +1,20 @@
 #pragma once
 
+// Header guard osigurava da se datoteka ukljuci samo jednom tijekom kompilacije, 
+// sprjecavajuci pogreske visestruke definicije.
 #include "game.h"
 
-// [Koncept 7: Organizacija izvornog koda - odvajanje prototipova u header]
+// Funkcije ispod cine CRUD sucelje. Koristenje 'const' pokazivaca jamci 
+// da funkcije nece nehoticno mijenjati izvorne podatke (sigurnost podataka).
+
+// Spremanje rezultata partije u binarnu datoteku.
 void save_result(const TrenutniRezultat* res);
+
+// citanje svih zapisa iz datoteke i prikaz ljestvice korisniku.
 void display_results();
+
+// Brisanje zapisa iz datoteke na temelju rednog broja (indeksa).
 void delete_single_result(int index);
+
+// Azuriranje broja pobjeda za odabranog igraca direktno u bazi.
 void rucni_update_pobjeda(const char* ime, int nove_pobjede);
